f8c4d360144607e26a7efc2e220e60d8f93062a9
Limbogeom <limbogeom@gmail.com>
2026-02-01 13:26:00 +0200

    production ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from .config import Config
 from .extensions import db, migrate
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object("app.config.Config")
     
     db.init_app(app)
     migrate.init_app(app, db)
--- app/config.py
+++ app/config.py
@@ -3,3 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
