b398483d104137364bf9fc5044eb88e03e9d9ccb
Limbogeom <limbogeom@gmail.com>
2026-02-03 22:09:08 +0200

    added indexes to created_at stored_at

--- app/models.py
+++ app/models.py
@@ -8,5 +8,5 @@ class Weather(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     humidity = db.Column(db.Float, nullable=False)
     light = db.Column(JSONB, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
-    stored_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now(), index=True)
+    stored_at = db.Column(db.DateTime(timezone=True), nullable=False, index=True)
